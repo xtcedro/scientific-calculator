79999e61c7a78b73f6d7bc3111147cb01b2a70b0
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-01-27 17:08:05 -0600

    Update

---
+++ setup-env.sh
@@ -0,0 +1,15 @@
+# Install React and ReactDOM
+npm install react react-dom
+
+# Install Webpack and Babel for development and building
+npm install -D @babel/core @babel/preset-env @babel/preset-react babel-loader \
+webpack webpack-cli webpack-dev-server css-loader style-loader
+
+# Install Chart.js and Math.js for your specific project needs
+npm install chart.js mathjs
+
+# (Optional) Install React Router for navigation
+npm install react-router-dom
+
+# (Optional) Install ESLint and Prettier for linting and formatting
+npm install eslint prettier --save-dev
